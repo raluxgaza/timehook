d291f4b29ebae26dccf00c1ef80dc14d361b2eb6
Raphael G Shekwolo <ralphsheks@gmail.com>
2010-11-18 11:10:38 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timehook::Application.config.secret_token = 'ca8eb21a1719474c40fca52f7333b9407d90d01ce97c38cd778440d60271d4349e8a4b787859a920752930f61dab5b5ab7f5ded082850f0bb65e1d2f2f9b3af5'
