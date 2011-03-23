9a9724d5d18e9568618c5b7efd7ea82400b9c351
Raphael G Shekwolo <ralphsheks@gmail.com>
2011-03-23 14:44:11 +0000

    Finished view activity feature

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,18 +1,12 @@
 class ActivitiesController < ApplicationController
   before_filter :authenticate
 
-  attr_accessor :f_date, :t_date
-  $from_date = nil
-  $to_date = nil
-  
   def index
-    #f_date = Date.civil(params[:from_date][:year].to_i, params[:from_date][:month].to_i, params[:from_date][:day].to_i)
-    #t_date = Date.civil(params[:to_date][:year].to_i, params[:to_date][:month].to_i, params[:to_date][:day].to_i)
+    f_date = Date.civil(params[:from_date][:year].to_i, params[:from_date][:month].to_i, params[:from_date][:day].to_i)
+    t_date = Date.civil(params[:to_date][:year].to_i, params[:to_date][:month].to_i, params[:to_date][:day].to_i)
 
-    @f_date = $from_date
-    @t_date = $to_date
-    @user_activities = Activity.find(:all, :conditions => ["user_id = ? and entry_date between ? and ? ", 
-                                     current_user, @f_date, @t_date], :order => "entry_date asc")
+    @summary_hours = Activity.find(:all, :conditions => ["user_id = ? and entry_date between ? and ? ", 
+                                     current_user, f_date, t_date], :order => "entry_date asc")
 
     @title = "Users activities"
   end
--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,2 +1,18 @@
 module ActivitiesHelper
+
+  def from_date=(f_date)
+    @from_date = f_date
+  end
+
+  def from_date
+    @from_date
+  end
+
+  def to_date=(t_date)
+    @to_date = t_date
+  end
+
+  def to_date
+    @to_date
+  end
 end
